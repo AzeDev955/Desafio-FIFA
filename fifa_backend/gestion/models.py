354c7f4234990f445d4b1d5c4e6ccf1fd5173de6
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-10-30 12:47:05 +0100

    Update models.py

--- fifa_backend/gestion/models.py
+++ fifa_backend/gestion/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    equipo = models.OneToOneField('Equipo', on_delete=models.SET_NULL, null=True, blank=True)
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}}"
+
+
+
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=150)
+
+    cartas = models.ManyToManyField(
+        'Carta',
+        blank=True
+    )
+    def __str__(self):
+        return f"{self.nombre}"
+
+class Carta(models.Model):
+    nombre = models.CharField(max_length=100)
+    pais = models.CharField(max_length=50, blank=True)
+    club = models.CharField(max_length=100, blank=True)
+    liga = models.CharField(max_length=100, blank=True)
+
+    ritmo = models.IntegerField()
+    tiro = models.IntegerField()
+    pase = models.IntegerField()
+    regate = models.IntegerField()
+    defensa = models.IntegerField()
+    fisico = models.IntegerField()
