a52cd43a6ce1f33a92ad9c36fa81aa2a8d5ad1cd
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-11-03 11:06:30 +0100

    Update views.py
    listar usuarios

--- fifa_backend/gestion/views.py
+++ fifa_backend/gestion/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from models import Usuario
+from django.http import JsonResponse
 
 # Create your views here.
+
+def listar_usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'listar_usuarios.html', {'usuarios': usuarios})
+    data = []
+    for usuario in usuarios:
+        data.append({
+            'id': usuario.id,
+            'nombre': usuario.nombre,
+            'apellido': usuario.apellido,
+            'email': usuario.email,
+            'equipo': usuario.equipo.nombre
+        })
+    return JsonResponse(data, safe=False)
