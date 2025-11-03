4d306cac20ded789c4a8ae113415e89da6313079
Aguila <joseaguila3012@gmail.com>
2025-11-03 11:13:06 +0100

    get cartas

--- fifa_backend/gestion/views.py
+++ fifa_backend/gestion/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from models import Usuario
-from django.http import JsonResponse
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from models import *
+from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def listar_usuarios(request):
     usuarios = Usuario.objects.all()
@@ -17,3 +17,23 @@ def listar_usuarios(request):
             'equipo': usuario.equipo.nombre
         })
     return JsonResponse(data, safe=False)
+
+# ------------------ LISTAR TODAS ------------------
+def listar_cartas(request):
+    if request.method != "GET":
+        return HttpResponseNotAllowed(['GET'])
+    data = list(Carta.objects.values())
+    return JsonResponse(data, safe=False)
+
+# ------------------ VER DETALLE ------------------
+def detalle_carta(request, id):
+    if request.method != "GET":
+        return HttpResponseNotAllowed(['GET'])
+    carta = get_object_or_404(Carta, id=id)
+    return JsonResponse({
+        "id": carta.id,
+        "nombre": carta.nombre,
+        "tipo": carta.tipo,
+        "posicion": carta.posicion,
+        "valoracion_general": carta.valoracion_general,
+    })
