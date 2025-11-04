ec7680a76d6f821ec1d29f61acae7a528736bb2d
AzeDev <alvarodaw2024@gmail.com>
2025-11-04 09:33:40 +0100

    Update crear_cartas.py
    switch logica

--- fifa_backend/gestion/management/commands/crear_cartas.py
+++ fifa_backend/gestion/management/commands/crear_cartas.py
@@ -3,7 +3,7 @@ from faker import Faker
 from django.core.management import BaseCommand
 from gestion.models import *
 
-from fifa_backend.gestion.models import CartaPortero
+from fifa_backend.gestion.models import CartaPortero, CartaJugador
 
 
 class Command(BaseCommand):
@@ -30,29 +30,86 @@ class Command(BaseCommand):
                     reflejos = random.randint(1, 100)
                     velocidad = random.randint(1, 100)
                     colocacion = random.randint(1, 100)
-                    carta = CartaPortero()
+                    carta = CartaPortero(
+                        nombre=nombre,
+                        pais=pais,
+                        club=club,
+                        liga=liga,
+                        tipo='POR',
+                        estirada=estirada,
+                        paradas=paradas,
+                        saque=saque,
+                        reflejos=reflejos,
+                        velocidad=velocidad,
+                        colocacion=colocacion,
+                    )
                     pass
                 case 'DEF':
+                    posicion_defensa = random.choice('DFC','LTI','LTD')
                     ritmo = random.randint(1, 100)
                     tiro = random.randint(1, 100)
                     pase = random.randint(1, 100)
                     regate = random.randint(1, 100)
                     defensa = random.randint(50, 100)
                     fisico = random.randint(1, 100)
+                    carta = CartaJugador(
+                        nombre=nombre,
+                        pais=pais,
+                        club=club,
+                        liga=liga,
+                        tipo='JUG',
+                        posicion = posicion_defensa,
+                        ritmo = ritmo,
+                        tiro = tiro,
+                        defensa = defensa,
+                        fisico = fisico,
+                        regate = regate,
+                        pase = pase,
+                    )
                     pass
                 case 'CEN':
+                    posicion_centrocampista = random.choice('MC', 'MI', 'MD')
                     ritmo = random.randint(1, 100)
                     tiro = random.randint(1, 100)
                     pase = random.randint(1, 100)
                     regate = random.randint(1, 100)
                     defensa = random.randint(1, 100)
                     fisico = random.randint(1, 100)
+                    carta = CartaJugador(
+                        nombre=nombre,
+                        pais=pais,
+                        club=club,
+                        liga=liga,
+                        tipo='JUG',
+                        posicion=posicion_centrocampista,
+                        ritmo=ritmo,
+                        tiro=tiro,
+                        defensa=defensa,
+                        fisico=fisico,
+                        regate=regate,
+                        pase=pase,
+                    )
                     pass
                 case 'DEL':
+                    posicion_delantero = random.choice('MP', 'DC')
                     ritmo = random.randint(1, 100)
                     tiro = random.randint(50, 100)
                     pase = random.randint(1, 100)
                     regate = random.randint(1, 100)
                     defensa = random.randint(1, 50)
                     fisico = random.randint(1, 100)
+                    carta = CartaJugador(
+                        nombre=nombre,
+                        pais=pais,
+                        club=club,
+                        liga=liga,
+                        tipo='JUG',
+                        posicion=posicion_delantero,
+                        ritmo=ritmo,
+                        tiro=tiro,
+                        defensa=defensa,
+                        fisico=fisico,
+                        regate=regate,
+                        pase=pase,
+                    )
                     pass
