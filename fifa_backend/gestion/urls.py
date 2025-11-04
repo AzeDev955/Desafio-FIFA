72a8a835765d48f64a66e248e0e434976d69a919
AzeDev <alvarodaw2024@gmail.com>
2025-11-04 01:08:47 +0100

    Update urls.py
    quick fix

--- fifa_backend/gestion/urls.py
+++ fifa_backend/gestion/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('usuarios/crear', views.crear_usuario, name='crear_usuario'),
     path('usuarios/actualizar/<int:user_id>', views.actualizar_usuario, name='actualizar_usuario'),
     path('usuarios/borrar/<int:user_id>', views.borrar_usuario, name='borrar_usuario'),
+    path('usuarios/<int:user_id>/asignar', views.asignar_equipo, name='asignar_equipo'),
     path('usuarios/<int:user_id>/equipo>', views.listar_equipo, name='listar_equipo'),
 
     path('cartas/', views.listar_cartas, name='listar_cartas'),
