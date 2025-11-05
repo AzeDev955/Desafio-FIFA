9f1ed3bd4448aefaaa33d84dbf7c026cc0d10f3b
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-11-05 20:03:55 +0100

    Update tests.py

--- fifa_backend/gestion/tests.py
+++ fifa_backend/gestion/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
-from models import *
+from .models import *
 import faker as fake
+
+from ..fifa_backend.wsgi import application
+
 # Create your tests here.
 fake = fake.Faker('es_ES')
 
 class UsuarioTest(TestCase):
-    def test_crear_usuario(self):
+    def test_crear_usuario_modelo(self):
         usuario = Usuario(
             nombre = 'TestNombre',
             apellido = 'TestApellido',
@@ -16,3 +19,43 @@ class UsuarioTest(TestCase):
         usuarioTest = Usuario.objects.get(email='TestCorreo@test.com')
 
         self.assertEqual(usuario.email,usuarioTest.email)
+
+    def test_listar_usuarios(self):
+        usuarioTest = Usuario(
+            nombre = 'TestNombre',
+            apellido = 'TestApellido',
+            email = 'TestCorreo@test.com'
+        )
+        usuarioTest.save()
+        respuesta = self.client.get('/gestion/usuarios/')
+
+        self.assertEqual(respuesta.status_code,200)
+        #La b es para pasarlo a bytes, que es lo que puede leer el assertIn
+        self.assertIn(b'TestNombre',respuesta.content)
+
+    def test_listar_un_usuario(self):
+        usuarioTest = Usuario(
+            nombre='TestNombre',
+            apellido='TestApellido',
+            email='TestCorreo@test.com'
+        )
+        usuarioTest.save()
+        idTest = usuarioTest.id
+        respuesta = self.client.get(f'/gestion/usuarios/{idTest}/')
+
+        self.assertEqual(respuesta.status_code, 200)
+
+        self.assertIn(b'TestNombre', respuesta.content)
+
+    def test_crear_usuario_vista(self):
+        datosUsuario = {
+            'nombre': 'TestNombre',
+            'apellido': 'TestApellido',
+            'email': 'TestCorreo@test.com'
+        }
+
+        respuesta = self.client.post('/gestion/usuarios/crear/',data=datosUsuario,content_type='application/json')
+
+        self.assertEqual(respuesta.status_code, 201)
+        self.assertEqual(respuesta.content.id,)
+
