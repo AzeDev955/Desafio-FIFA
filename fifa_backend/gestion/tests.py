d73bbaa6477326bb07f197b9cb1eb300fc96bf4b
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-11-05 19:18:57 +0100

    Update tests.py

--- fifa_backend/gestion/tests.py
+++ fifa_backend/gestion/tests.py
@@ -1,7 +1,18 @@
 from django.test import TestCase
 from models import *
+import faker as fake
 # Create your tests here.
-
+fake = fake.Faker('es_ES')
 
 class UsuarioTest(TestCase):
     def test_crear_usuario(self):
+        usuario = Usuario(
+            nombre = 'TestNombre',
+            apellido = 'TestApellido',
+            email = 'TestCorreo@test.com'
+        )
+        usuario.save()
+
+        usuarioTest = Usuario.objects.get(email='TestCorreo@test.com')
+
+        self.assertEqual(usuario.email,usuarioTest.email)
