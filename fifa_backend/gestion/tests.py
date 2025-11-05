cbf65646c21e599b13ccda39e7bcd11e85e32e7c
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-11-05 21:19:11 +0100

    Update tests.py
    delete usuario, asignar equipo y listar equipo

--- fifa_backend/gestion/tests.py
+++ fifa_backend/gestion/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from .models import *
 import faker as fake
-
+import random
 # Create your tests here.
 fake = fake.Faker('es_ES')
 
@@ -82,6 +82,98 @@ class UsuarioTest(TestCase):
         self.assertEqual(respuesta.status_code, 200)
         usuarioActualizado = Usuario.objects.get(id=usuarioTest.id)
         self.assertEqual('TestCorreo2@test.com',usuarioActualizado.email)
+
+    def test_borrar_usuario(self):
+        usuarioTest = Usuario(
+            nombre='TestNombre',
+            apellido='TestApellido',
+            email='TestCorreo@test.com'
+        )
+        usuarioTest.save()
+        respuesta = self.client.delete(f'/gestion/usuarios/borrar/{usuarioTest.id}')
+        jsonRespuestaVista = {
+                'exito': True,
+                'mensaje': 'Usuario borrado correctamente'
+            }
+        #el with crea un bloque de codigo que espera algo en concreto, si salta el error bien, si no salta sale mal el test
+        with self.assertRaises(Usuario.DoesNotExist):
+            Usuario.objects.get(email='TestCorreo@test.com')
+        self.assertEqual(respuesta.json(), jsonRespuestaVista)
+
+class EquipoTest(TestCase):
+    def test_asignar_equipo(self):
+        usuarioTest = Usuario(
+            nombre='TestNombre',
+            apellido='TestApellido',
+            email='TestCorreo@test.com'
+        )
+        usuarioTest.save()
+        #Creacion de cartas para poder testear la funcion
+
+        for i in range(10):
+            CartaPortero.objects.create(
+                nombre=f"PorteroTest {i}",
+                posicion='POR',
+                estirada=50, paradas=50, saque=50,
+                reflejos=50, velocidad=50, colocacion=50
+            )
+        posiciones_def = ['DFC', 'LTI', 'LTD']
+        posiciones_cen = ['MC', 'MI', 'MD']
+        posiciones_del = ['DC', 'MP']
+
+        for i in range(10):  # Crear 10 Defensas
+            CartaJugador.objects.create(
+                nombre=f"DefensaTest {i}",
+                posicion=random.choice(posiciones_def),
+                ritmo=50, tiro=50, pase=50,
+                regate=50, defensa=50, fisico=50
+            )
+        for i in range(10):  # Crear 10 Centrocampistas
+            CartaJugador.objects.create(
+                nombre=f"CentroTest {i}",
+                posicion=random.choice(posiciones_cen),
+                ritmo=50, tiro=50, pase=50,
+                regate=50, defensa=50, fisico=50
+            )
+        for i in range(10):  # Crear 10 Delanteros
+            CartaJugador.objects.create(
+                nombre=f"DelanteroTest {i}",
+                posicion=random.choice(posiciones_del),
+                ritmo=50, tiro=50, pase=50,
+                regate=50, defensa=50, fisico=50
+            )
+
+
+        respuesta = self.client.post(f'/gestion/usuarios/{usuarioTest.id}/asignar')
+        self.assertEqual(respuesta.status_code, 200)
+        usuarioTestEquipo = Usuario.objects.get(email='TestCorreo@test.com')
+        respuestaVista = {
+                'exito': True,
+                'equipo': usuarioTestEquipo.equipo.nombre,
+                'id equipo': usuarioTestEquipo.equipo.id
+                                 }
+        self.assertEqual(respuesta.json(),respuestaVista)
+
+    def test_listar_equipo(self):
+        cartaTest = CartaPortero.objects.create(
+            nombre="PorteroTest 1", posicion='POR', estirada=50, paradas=50,
+            saque=50, reflejos=50, velocidad=50, colocacion=50
+        )
+        equipoTest = Equipo.objects.create(nombre="Equipo de Prueba")
+        equipoTest.cartas.add(cartaTest)
+        usuarioTest = Usuario(
+            nombre='TestNombre',
+            apellido='TestApellido',
+            email='TestCorreo@test.com',
+            equipo=equipoTest
+        )
+        usuarioTest.save()
+        respuesta = self.client.get(f'/gestion/usuarios/{usuarioTest.id}/equipo')
+
+        self.assertEqual(respuesta.status_code,200)
+        datos_respuesta = respuesta.json()
+        self.assertEqual(datos_respuesta['equipo'],equipoTest.nombre)
+
 
 
 
